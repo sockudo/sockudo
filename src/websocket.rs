99ade21c1d43300944975a5254ae7decf572e6b2
PTFOPlayer <patryk041103@gmail.com>
2025-05-16 10:26:08 +0200

    fix: fixing rng deprecation warnings

--- src/websocket.rs
+++ src/websocket.rs
@@ -67,14 +67,14 @@ impl SocketId {
     }
 
     pub(crate) fn generate_socket_id() -> String {
-        let mut rng = rand::thread_rng(); // Get a random number generator
+        let mut rng = rand::rng(); // Get a random number generator
 
         // Define min and max as u64, since Rust requires specifying the integer type
         let min: u64 = 0;
         let max: u64 = 10000000000;
 
         // Rust's rand crate handles generating a random number between min and max differently
-        let mut random_number = |min: u64, max: u64| -> u64 { rng.gen_range(min..=max) };
+        let mut random_number = |min: u64, max: u64| -> u64 { rng.random_range(min..=max) };
 
         // Format the random numbers into a String with a dot separator
         format!("{}.{}", random_number(min, max), random_number(min, max))
