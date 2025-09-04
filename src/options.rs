4a15a89abc4c8456eb4878c49980b459f12c5dc7
Marc Espiard <marc@gethomesafe.com>
2025-09-05 06:52:19 +1200

    Moved option

--- src/options.rs
+++ src/options.rs
@@ -973,10 +973,6 @@ impl ServerOptions {
                 "RateLimiter Backend",
             );
         }
-        // --- Adapter: Redis ---
-        if let Ok(prefix) = std::env::var("RATE_LIMITER_REDIS_PREFIX") {
-            self.rate_limiter.redis.prefix = Some(prefix);
-        }
 
         // --- Database: Redis ---
         if let Ok(host) = std::env::var("DATABASE_REDIS_HOST") {
@@ -1107,6 +1103,9 @@ impl ServerOptions {
             "RATE_LIMITER_WS_WINDOW_SECONDS",
             self.rate_limiter.websocket_rate_limit.window_seconds,
         );
+        if let Ok(prefix) = std::env::var("RATE_LIMITER_REDIS_PREFIX") {
+            self.rate_limiter.redis.prefix = Some(prefix);
+        }
 
         // --- Queue: Redis ---
         self.queue.redis.concurrency =
