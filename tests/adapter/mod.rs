8f7d8426ac9ad1f64b7090733372793d9a9a9006
Marc Espiard <marc@gethomesafe.com>
2025-09-10 14:34:32 +1200

    Horizontal adapters

--- tests/adapter/mod.rs
+++ tests/adapter/mod.rs
@@ -1,2 +1,11 @@
 pub mod handler;
 pub mod transports;
+
+#[cfg(test)]
+mod horizontal_adapter_helpers;
+
+#[cfg(test)]
+mod horizontal_adapter_base_test;
+
+#[cfg(test)]
+mod horizontal_adapter_integration;
