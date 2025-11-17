cea48a06d9639684442e1e08ec466b78855b008b
Marc Espiard <m.espiard@gmail.com>
2025-11-18 11:18:12 +1300

    Prevent sending close frame when connection is already shutting down

--- src/websocket.rs
+++ src/websocket.rs
@@ -311,12 +311,14 @@ impl MessageSender {
                 }
             }
 
-            // Try to close gracefully
-            if let Err(e) = socket
-                .write_frame(Frame::close(1000, b"Normal closure"))
-                .await
-            {
-                Self::log_connection_error(&e, SocketOperation::SendCloseFrame, frame_count, true);
+            // Try to close gracefully - only if we haven't already sent a close frame
+            if !is_shutting_down {
+                if let Err(e) = socket
+                    .write_frame(Frame::close(1000, b"Normal closure"))
+                    .await
+                {
+                    Self::log_connection_error(&e, SocketOperation::SendCloseFrame, frame_count, true);
+                }
             }
         });
 
@@ -398,12 +400,14 @@ impl MessageSender {
                 }
             }
 
-            // Try to close gracefully
-            if let Err(e) = socket
-                .write_frame(Frame::close(1000, b"Normal closure"))
-                .await
-            {
-                Self::log_connection_error(&e, SocketOperation::SendCloseFrame, frame_count, true);
+            // Try to close gracefully - only if we haven't already sent a close frame
+            if !is_shutting_down {
+                if let Err(e) = socket
+                    .write_frame(Frame::close(1000, b"Normal closure"))
+                    .await
+                {
+                    Self::log_connection_error(&e, SocketOperation::SendCloseFrame, frame_count, true);
+                }
             }
         });
 
